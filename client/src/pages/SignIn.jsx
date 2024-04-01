918c2996fe8ac9fd78457863aec64facead01ea9
manu <manuabraham165@gmail.com>
2024-04-01 23:18:15 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {signInStart, signInSuccess,signInFailure} from '../redux/user/userSlice'
 
 export default function SignIn() {
 
   const [formData,setformData]= useState({})  
-  const [error,setError]= useState(null)
+  const {loading, error} = useSelector((state) =>state.user)
   const navigate=useNavigate()
+  const dispatch=useDispatch()
 
-  const [loading,setLoading]=useState(false)
   const handleChange= (e) =>{
     setformData({
       ...formData,
@@ -19,7 +21,7 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      setLoading(true)
+      dispatch(signInStart)
       const res=await fetch('/api/auth/signin',
       {
         method:'POST',
@@ -30,16 +32,13 @@ export default function SignIn() {
       });
       const data=await res.json();
       if(data.success === false){
-        setError(data.message)
-        setLoading(false)
+        dispatch(signInFailure(data.message))
         return
       }
-      setLoading(false)
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate('/')
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message))
     }
     
     console.log(data)
@@ -57,7 +56,7 @@ export default function SignIn() {
       <div className='flex gap-2 mt-5'>
         <p>Don't have an account?</p>
         <Link to={"/sign-up"}>
-          <span className='text-blue-700'>Sign in</span>
+          <span className='text-blue-700'>Sign Up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
