f335a23d6b4e42466349ff86de4c2f48a225f1a8
manu <manuabraham165@gmail.com>
2024-04-10 13:42:25 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,10 @@ import { useRef, useState, useEffect } from 'react'
 import { useSelector,useDispatch } from 'react-redux'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
-import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure, 
+  deleteUserStart, deleteUserFailure, deleteUserSuccess, 
+  signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice';
+import {Link} from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -146,7 +149,12 @@ export default function Profile() {
         rounded-lg p-3 uppercase hover:opacity-95 
         disabled:opacity-80'>
           {loading ? 'Loading...' : 'Update' }
-          </button>  
+        </button>
+        <Link className='bg-green-700 
+        text-white p-3 rounded-lg 
+        uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link> 
 
       </form>
       <div className='flex justify-between mt-5'>
