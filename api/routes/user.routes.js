8c67ae16737cd935607c2372c956ba2972637e82
manu <manuabraham165@gmail.com>
2024-04-23 11:36:54 +0530

    create update listing API route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -9,4 +9,5 @@ router.post('/update/:id', verifyToken ,updateUser);
 router.delete('/delete/:id', verifyToken , deleteUser);
 router.get('/listings/:id',verifyToken, getUserListing);
 
+
 export default router;
